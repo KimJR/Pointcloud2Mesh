f5387d90cf74e31a2a2e3c58fc07bfce2054735a
Alexander <alex.ngbr@googlemail.com>
2022-02-11 14:37:49 +0100

    Normal map and position map support for ColorToUVIndex
    This script does not work for me due to "mesh.vertex_normal" giving an error, but it might work for others.

--- VertexColorToTexture/ColorToTexture.py
+++ VertexColorToTexture/ColorToTexture.py
@@ -1,10 +1,11 @@
 import os.path
-import numba
-from numba import jit, njit, prange
+#import numba
+#from numba import jit, njit, prange
 import numpy as np
 import vectormath
 import trimesh
 from trimesh import visual
+from trimesh import base
 import xatlas
 from PIL import Image
 
@@ -27,26 +28,19 @@ def get_texture_from_vertex_color(input_file: str, textureWidth: int = 1024):
     vertex_colors = mesh.visual.vertex_colors # contains the color per vertex before xatlas conversion
     vertex_normals = mesh.vertex_normals  # contains normal vertices
 
-    print(str(len(uvs)) + ", " + vmapping[0])
-
     new_vertices = [0]*len(uvs) # vertices after xatlas conversion
     new_colors = [0]*len(uvs) # contains the color of each vertex after xatlas conversion (after generating UVs)
     new_normals = [0]*len(uvs) # contains the normals of each vertex after xatlas conversion (after generating UVs)
     for i in range(0, len(uvs)):
-        new_vertices = vertices[vmapping]
-        new_colors[i] = vertex_colors[vmapping]
-        new_normals[i] = vertex_normals[vmapping]
+        new_vertices[i] = vertices[vmapping[i]]
+        new_colors[i] = vertex_colors[vmapping[i]]
+        new_normals[i] = vertex_normals[vmapping[i]]
 
     ### UV COORDINATES (of Vertices)
-    pixel_position_array = create_texture(new_vertices, indices, textureWidth, uvs, new_colors, new_normals)
-    # TODO store as png with numpy, imageIo
-    trimesh.visual.color.ColorVisuals(mesh, None, pixel_position_array)
-    im = Image.fromarray(pixel_position_array)
-    im.save("./VertexColorToTexture/texture/texture_map.png")
+    pixel_color_array = create_texture(new_vertices, indices, textureWidth, uvs, new_colors, new_normals)
 
-
-@jit(nopython=True, cache=True, parallel=True)
-def create_texture(vertices, faces, textureWidth, uv_coordinates, new_colors, new_normals):
+#@jit(nopython=True, cache=True, parallel=True)
+def create_texture(new_vertices, faces, textureWidth, uv_coordinates, new_colors, new_normals):
     ### INIT storing arrays
     textureShape = (textureWidth, textureWidth, 3)
     pixel_color_array = np.zeros(textureShape, dtype=np.uint8)  # the texture to which the color will be mapped
@@ -90,12 +84,12 @@ def create_texture(vertices, faces, textureWidth, uv_coordinates, new_colors, ne
                 # y = a * y1 + b * y2 + c * y3 --> (y - c*y3 - a*y1)/y2 = b
                 #                              --> ((x - c*x3 - ((y - c*y3 - a*y1)/y2)*x2)/x1 = a
                 # a + b + c = 1
-                # a = ((y2 - y3) * (xNorm - x3) + (x3 - x2) * (yNorm - y3)) / (
-                #            (y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3))
-                # b = ((y3 - y1) * (xNorm - x3) + (x1 - x3) * (yNorm - y3)) / (
-                #            (y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3))
-                a = ((x1 - x3) * (y - y3) - (y1 - y3) * (x - x3)) # https://stackoverflow.com/questions/2049582/how-to-determine-if-a-point-is-in-a-2d-triangle
-                b = ((x2 - x1) * (y - y1) - (y2 - y1) * (x - x1))
+                a = ((y2 - y3) * (xNorm - x3) + (x3 - x2) * (yNorm - y3)) / (
+                           (y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3))
+                b = ((y3 - y1) * (xNorm - x3) + (x1 - x3) * (yNorm - y3)) / (
+                           (y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3))
+                #a = ((x1 - x3) * (y - y3) - (y1 - y3) * (x - x3)) # https://stackoverflow.com/questions/2049582/how-to-determine-if-a-point-is-in-a-2d-triangle
+                #b = ((x2 - x1) * (y - y1) - (y2 - y1) * (x - x1))
                 c = 1 - a - b
 
                 #print(a, " ", b, " ", c)
@@ -103,22 +97,30 @@ def create_texture(vertices, faces, textureWidth, uv_coordinates, new_colors, ne
                 if 0 <= a and 0 <= b and 0 <= c and (a + b + c) <= 1:
                     # color the pixel depending on the lerp between the three pixels
                     # new_colors might has shape of (lenOfVertices, 4) if it stores RGBA values --> only use RGB
-                    pixel_color = a * new_colors[v0, :3] + b * new_colors[v1, :3] + c * new_colors[v2, :3]
+                    pixel_color = a * new_colors[v0][:3] + b * new_colors[v1][:3] + c * new_colors[v2][:3]
                     # print("pixel_color= ", pixel_color)
-                    x_i = x - sqxStart
-                    y_i = y - sqyStart
-                    pixel_color_array[x_i, y_i] = pixel_color
+                    pixel_color_array[x, y] = pixel_color
                     #  TODO write in hdr,
-                    pColor = (a * new_normals[v0] + b * new_normals[v1] + c * new_normals[v2])/2
-                    pixel_normal_color = [int(255*(pColor[0] + 0.5)), int(255*(pColor[1] + 0.5)), int(255*(pColor[2] + 0.5))]
+                    normal_color = (a * new_normals[v0] + b * new_normals[v1] + c * new_normals[v2])/2
+                    pixel_normal_color = [int(255*(normal_color[0] + 0.5)), int(255*(normal_color[1] + 0.5)), int(255*(normal_color[2] + 0.5))]
                     pixel_normal_array[x_i, y_i] = pixel_normal_color
 
-                    pixel_position_color = int(position_color_range * a * vertices[v0]) + int(position_color_range *b * vertices[v1]) + int(position_color_range * (c * vertices[v2]))
-                    pixel_position_array[x_i, y_i] = pixel_position_color
-        print(str(i) + " from " + str(len(faces)))
-    return pixel_position_array
+                    pixel_position_color = position_color_range * a * new_vertices[v0] + position_color_range * b * new_vertices[v1] + position_color_range * c * new_vertices[v2]
+                    pixel_position_array[x, y] = pixel_position_color
+        print(str(i) + ") - uvs: " + str([x1, y1]) + ", color: " + str(pixel_color) + ", pixel: (" + str(x) + ", " + str(y) + ")")
+    # TODO store as png with numpy, imageIo
+    im = Image.fromarray(pixel_color_array)
+    im.save(os.getcwd() + "\\VertexColorToTexture\\texture\\texture_map.png")
 
-get_texture_from_vertex_color("C:\\Users\\Alexander\\Documents\\GitHub\\Pointcloud2Mesh\\models\\color_to_uv_index_testfiles\\meshAfterPoisson")
+    im_n = Image.fromarray(pixel_normal_array)
+    im_n.save(os.getcwd() + "\\VertexColorToTexture\\texture\\normal_map.png")
+
+    im_p = Image.fromarray(pixel_position_array)
+    im_p.save(os.getcwd() + "\\VertexColorToTexture\\texture\\position_map.png")
+    
+    return pixel_color_array
+
+#get_texture_from_vertex_color("C:\\Users\\Alexander\\Documents\\GitHub\\Pointcloud2Mesh\\models\\color_to_uv_index_testfiles\\meshAfterPoisson")
 
 if __name__ == "__main__":
     get_texture_from_vertex_color(os.getcwd() + "/models/mesh")
